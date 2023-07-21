6a4b0876152def2fc1ef202eb92d0e8379231fa4
YarikSokolov <90636912+YarikSokolov@users.noreply.github.com>
2023-07-21 11:47:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let game = document.getElementById("game")
+let shootbutton = document.getElementById("shootbutton")
+game.style.height = window.innerWidth + "px"
+shootbutton.onclick = function (event) {
+    console.log("shot!")
+}
